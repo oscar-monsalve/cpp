d5e9237599b135866a1e5977ea8116baf663d51c
Oscar Monsalve <oscarmonsalvec5@gmail.com>
2026-09-06 14:14:30 -0500

    added solutions

--- pointers_and_mem_mgmt/02-swap-with-pointers/02-swap-with-pointers.cpp
+++ pointers_and_mem_mgmt/02-swap-with-pointers/02-swap-with-pointers.cpp
@@ -7,23 +7,33 @@
 // - Call the function from `main` and print the values before and after the swap.
 // - Handle null pointers without crashing.
 
-#include <iostream>
+#include <print>
 
 void swapValues(int* first, int* second) {
+    // Check if the pointers are not null
     if (first == nullptr || second == nullptr) {
-        std::cout << "Null pointer found: " << first << second << "\n";
-        std::cout << "Exiting safely." << first << second << "\n";
+        std::println("Cannot swap null pointers");
         return;
     }
 
-    int* x = second;
+    int temp = *first;  // Dereferencing the pointer of the 'first' number to access its value
+    *first = *second;
+    *second = temp;
 }
 
 int main () {
     int first = 1;
     int second = 9;
 
-    std::cout << "First before: " << first << "\n";
+    std::println("Swapping numbers passing pointers\n");
+
+    std::println("'First' before swap: {}", first);
+    std::println("'Second' before swap: {}\n", second);
+
+    swapValues(&first, &second);
+
+    std::println("'First' after swap: {}", first);
+    std::println("'Second' after swap: {}", second);
 
     return 0;
 }
