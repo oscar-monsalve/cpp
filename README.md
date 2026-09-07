973bf00a90d13c6e0cb90fbf8da6d4453d9d6c3a
Oscar Monsalve <oscarmonsalvec5@gmail.com>
2026-09-06 22:16:41 -0500

    updated .gitignore

--- README.md
+++
@@ -1,3 +0,0 @@
-# Exercises in C++
-
-The C++26 standard is used to solve the exercises.
--- scientific_notation/scientific_notation.cpp
+++
@@ -1,9 +0,0 @@
-#include <print>
-
-int main() {
-    int power = 3e3;
-
-    std::println("The power operation is:\n{}", power);
-
-    return 0;
-}
