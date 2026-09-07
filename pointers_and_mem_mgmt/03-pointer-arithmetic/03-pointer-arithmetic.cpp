29d3d0c3ef8deb723c26cad3c01c2c9f65ff7ccf
oscar-monsalve <oscarmonsalvec5@gmail.com>
2026-09-06 19:08:53 -0500

    added progress ex 03

--- pointers_and_mem_mgmt/03-pointer-arithmetic/03-pointer-arithmetic.cpp
+++ pointers_and_mem_mgmt/03-pointer-arithmetic/03-pointer-arithmetic.cpp
@@ -8,50 +8,73 @@
 // - Pass the beginning and end pointers to the calculation functions.
 // - Do not use expressions such as `array[index]` while traversing the array.
 
-
 #include <print>
+#include <iterator>
 
-int arrMinValue(int* startPtr) {
-    return *startPtr;
+int arrMinValue(const int* startPtr, const int* endPtr) {
+    int min = *startPtr;
+    for (const int* current=startPtr; current < endPtr; ++current) {
+        if (*current < min) {
+        }
+    }
 }
 
-int arrMaxValue(int* endPtr) {
-    return *endPtr - 1;
-}
+// int arrMaxValue(const int* startPtr, const int* endPtr) {
+//     for (const int* current=startPtr; current < endPtr; ++current) {
+//     }
+// }
 
-int arrSum(int* startPtr, int* endPtr) {
+int arrSum(const int* startPtr, const int* endPtr) {
     int sum = 0;
 
-    // Loop through the array as long as startPtr has not reached endPtr
-    while (startPtr < endPtr) {
-        sum += *startPtr;
+    // Loop through the array until 'current' reaches 'endPtr'
+    for (const int* current=startPtr; current < endPtr; ++current) {
+        sum += *current;
     }
 
     return sum;
 }
 
 int main () {
-    int numberArray[] = {1, 2, 3, 4, 5};
-    int arrayLength = std::size(numberArray);
+    int numberArray1[] = {1, 2, 3, 4, 5};
+    int numberArray2[] = {3, 1, 5, 2, 1, 6};  // sum: 18
 
-    // Using the array name to create a pointer automatically decays to an int*. Using '&numberArray' is wrong:
-    // it creates an 'int(*)[5]' type
-    int* startPtr = numberArray;
-    int* endPtr = numberArray + arrayLength;  // One past the end of the array
+    int arrayLength1 = std::size(numberArray1);
+    int arrayLength2 = std::size(numberArray2);
 
+    // Using the array name to create a pointer automatically decays to an int*. Using '&numberArray' in this
+    // case is not correct because it creates an 'int(*)[5]' type, and a 'const int*' type is needed.
+    const int* startPtr1 = numberArray1;
+    const int* endPtr1 = numberArray1 + arrayLength1;  // Past element of the array
+
+    const int* startPtr2 = numberArray2;
+    const int* endPtr2 = numberArray2 + arrayLength2;
+
+    // Array 1
+    std::println("Array 1: {}:", numberArray1);
+    std::println("    Size: {}", arrayLength1);
     // Print the memory address of the array (first value). The raw pointer must be casted to a generic pointer
-    // type 'void*' or 'const void*' with 'static_cast'
-    // to allow the println function to show the hex address.
-    std::println("Array size: {}\n", arrayLength);
+    // type 'void*' or 'const void*' with 'static_cast' to allow the println function to show the hex address.
+    std::println("    Mem. address: {}", static_cast<const void*>(startPtr1));
+    std::println("    Start value: {}", *startPtr1);
+    std::println("    End value: {}", *(endPtr1 - 1));
+    std::println("    Calculations:");
+    // std::println("Min. value: {}", arrMinValue(startPtr));
+    // std::println("Max. value: {}", arrMaxValue(endPtr));
+    std::println("        - Sum of all array values: {}\n", arrSum(startPtr1, endPtr1));
 
-    std::println("Array pointer mem. address: {}", static_cast<void*>(startPtr));
-    std::println("Start array pointer value: {}", *startPtr);
-    std::println("End array pointer value: {}\n", *endPtr);
 
-    std::println("Solution:");
-    std::println("Array min. value: {}", arrSum(startPtr, endPtr));
-    std::println("Array max. value: {}", arrMinValue(startPtr));
-    std::println("Array sum of values: {}", arrMaxValue(endPtr));
+    // Array 2
+    std::println("Array 2: {}:", numberArray2);
+    std::println("    Size: {}", arrayLength2);
+    std::println("    Mem. address: {}", static_cast<const void*>(startPtr2));
+    std::println("    Start value: {}", *startPtr2);
+    std::println("    End value: {}", *(endPtr2 - 1));
+    std::println("    Calculations:");
+    // std::println("Min. value: {}", arrMinValue(startPtr2));
+    // std::println("Max. value: {}", arrMaxValue(endPtr2));
+    std::println("        - Sum of all array values: {}\n", arrSum(startPtr2, endPtr2));
+
 
     return 0;
 }
